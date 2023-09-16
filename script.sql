4b1eb228ef21e91a9eb7bf0dcbf0dccc97e6582f
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-16 13:28:54 -0300

    ap10(repeticao): codigo 2.4.4

--- script.sql
+++ script.sql
@@ -1,17 +1,47 @@
--- IF/EXIST
+-- CONTINUE
 DO $$
 DECLARE
-	contador INT := 1;
+	contador INT := 0;
 BEGIN
 	LOOP
-		RAISE NOTICE 'Contador: %', contador;
 		contador := contador + 1;
-		IF contador > 10 THEN
-			EXIT;
+		EXIT WHEN contador > 100;
+		-- ignorando as iterações em que contador é múltiplo de 7
+		IF contador % 7 = 0 THEN
+			CONTINUE;
 		END IF;
+		CONTINUE WHEN contador % 11 = 0;
+		RAISE NOTICE '%', contador;
 	END LOOP;
 END;
 $$
+-- EXIT WHEN
+-- DO $$
+-- DECLARE
+-- 	contador INT := 1;
+-- BEGIN
+-- 	LOOP
+-- 		RAISE NOTICE 'Contador: %', contador;
+-- 		contador := contador + 1;
+-- 		EXIT WHEN contador > 10;
+-- 	END LOOP;
+-- END;
+-- $$
+
+-- IF/EXIT
+-- DO $$
+-- DECLARE
+-- 	contador INT := 1;
+-- BEGIN
+-- 	LOOP
+-- 		RAISE NOTICE 'Contador: %', contador;
+-- 		contador := contador + 1;
+-- 		IF contador > 10 THEN
+-- 			EXIT;
+-- 		END IF;
+-- 	END LOOP;
+-- END;
+-- $$
 
 
 -- DO $$
