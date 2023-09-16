92fa99c0411156d94485779db581b44ff7be211a
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-16 13:59:01 -0300

    ap10(repeticao): codigo 2.4.7

--- script.sql
+++ script.sql
@@ -1,20 +1,73 @@
+-- DO $$
+-- DECLARE
+-- 	nota INT;
+-- 	media NUMERIC (10, 2) := 0;
+-- 	contador INT := 0;
+-- BEGIN
+-- 	--[-1, 10] -1 será nosso sentinela
+-- 	SELECT valor_aleatorio_entre(0, 11) -1 INTO nota;
+-- 	WHILE nota >= 0 LOOP
+-- 		RAISE NOTICE '%', nota;
+-- 		media := media + nota; --acumulador
+-- 		contador := contador + 1;
+-- 		SELECT valor_aleatorio_entre(0, 11) -1 INTO nota;
+-- 	END LOOP;
+-- 	IF contador > 0 THEN -- se tem pelo menos uma nota
+-- 		RAISE NOTICE 'Média: %', media / contador;
+-- 	ELSE
+-- 		RAISE NOTICE 'Nenhuma nota gerada';
+-- 	END IF;
+-- END;
+-- $$
+
+-- DO $$
+-- DECLARE 
+-- 	i INT;
+-- 	j INT;
+-- BEGIN
+-- 	i := 0;
+-- 	-- esse é um rótulo
+-- 	<<externo>>
+-- 	LOOP
+-- 		i := i + 1;
+-- 		EXIT WHEN i > 10;
+-- 		j := 1;
+-- 		<<interno>>
+-- 		LOOP
+-- 			RAISE NOTICE '%, %', i, j;
+-- 			j := j + 1;
+-- 			EXIT WHEN j > 5;
+-- 			EXIT externo WHEN j > 5;
+-- 		END LOOP;
+-- 	END LOOP;
+-- END;
+-- $$
+--1 6 N
+--1 5 S
+--1 4 S
+--1 3 S
+--1 2 S
+--1 1 S
+--0 1, 0 2 N
+
+
 -- CONTINUE
-DO $$
-DECLARE
-	contador INT := 0;
-BEGIN
-	LOOP
-		contador := contador + 1;
-		EXIT WHEN contador > 100;
-		-- ignorando as iterações em que contador é múltiplo de 7
-		IF contador % 7 = 0 THEN
-			CONTINUE;
-		END IF;
-		CONTINUE WHEN contador % 11 = 0;
-		RAISE NOTICE '%', contador;
-	END LOOP;
-END;
-$$
+-- DO $$
+-- DECLARE
+-- 	contador INT := 0;
+-- BEGIN
+-- 	LOOP
+-- 		contador := contador + 1;
+-- 		EXIT WHEN contador > 100;
+-- 		-- ignorando as iterações em que contador é múltiplo de 7
+-- 		IF contador % 7 = 0 THEN
+-- 			CONTINUE;
+-- 		END IF;
+-- 		CONTINUE WHEN contador % 11 = 0;
+-- 		RAISE NOTICE '%', contador;
+-- 	END LOOP;
+-- END;
+-- $$
 -- EXIT WHEN
 -- DO $$
 -- DECLARE
